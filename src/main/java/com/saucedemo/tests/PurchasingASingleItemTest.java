8125461d3f481bfc51fa001d03610fdfed7c0d0d
hanschehaiber <hans.chehaiber@gmail.com>
2022-01-31 22:11:20 -0800

    First TestCase

---
+++ src/main/java/com/saucedemo/tests/PurchasingASingleItemTest.java
@@ -0,0 +1,15 @@
+package com.saucedemo.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class PurchasingASingleItemTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+
+}
