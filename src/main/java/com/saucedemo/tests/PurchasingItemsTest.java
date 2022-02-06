cd66e62c1c709600944dcafe27ffebd46e9867bd
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-05 22:10:59 -0800

    Updated test to check for presence of credentials on the test application's front page.

--- src/main/java/com/saucedemo/tests/PurchasingItemsTest.java
+++ src/main/java/com/saucedemo/tests/PurchasingItemsTest.java
@@ -1,20 +1,30 @@
 package com.saucedemo.tests;
 
-import com.saucedemo.pages.SauceDemoLoginPage;
+import com.saucedemo.pages.LoginPage;
+import com.saucedemo.pages.ProductsPage;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 
 public class PurchasingItemsTest extends TestBase {
 
     @Test
-    public void test() {
+    public void purchasingASingleItemTest() {
+        SoftAssert softly = new SoftAssert();
 
         driver.get(prop.getProperty("url"));
-        SauceDemoLoginPage sauceDemoLoginPage = new SauceDemoLoginPage(driver);
-        sauceDemoLoginPage.inputUsername("standard_user");
-        sauceDemoLoginPage.inputPassword("secret_sauce");
-        sauceDemoLoginPage.clickLogin(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.inputUsername("standard_user");
+        loginPage.inputPassword("secret_sauce");
+        softly.assertTrue(loginPage.checkIfUsernamesArePresent());
+        softly.assertTrue(loginPage.checkIfPasswordIsPresent());
 
+
+        ProductsPage productsPage = loginPage.clickLogin(driver);
+
+
+
+        softly.assertAll();
     }
 
 
