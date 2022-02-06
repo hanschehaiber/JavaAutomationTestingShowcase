033e2b3bf900f628ce840445f518b0fba00156e2
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-05 22:47:21 -0800

    Now able to get the price of an item in the list by its index

--- src/main/java/com/saucedemo/pages/ProductsPage.java
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -9,13 +9,22 @@ import java.util.List;
 public class ProductsPage {
     private WebDriver driver;
 
-    By inventoryItems = By.cssSelector("[class='inventory_list'] > div");
+    private static final By inventoryItems = By.cssSelector("[class='inventory_list'] > div");
+    private static final By itemPrice = By.cssSelector("[class='inventory_item_price']");
     List<WebElement> inventoryList;
 
     public ProductsPage(WebDriver driver) {
         this.driver = driver;
-        inventoryList = driver.findElements(inventoryItems);
+        inventoryList = this.driver.findElements(inventoryItems);
     }
+
+    public String getPriceByIndex(int index) {
+        return inventoryList.get(index).findElement(itemPrice).getText();
+    }
+
+
+
+
 
 
 }
