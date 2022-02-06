903d70a7b05a1494aa5614877fe472799792ee61
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-05 20:27:46 -0800

    Added Password Input field text

--- src/main/java/com/saucedemo/pages/SauceDemoLoginPage.java
+++ src/main/java/com/saucedemo/pages/SauceDemoLoginPage.java
@@ -7,9 +7,14 @@ public class SauceDemoLoginPage {
     WebDriver driver;
 
     By username = By.xpath("//input[@data-test='username']");
+    By password = By.id("password");
 
     public SauceDemoLoginPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void inputPassword(String passwordText) {
+        driver.findElement(password).sendKeys(passwordText);
     }
 
     public void inputUsername(String usernameText) {
