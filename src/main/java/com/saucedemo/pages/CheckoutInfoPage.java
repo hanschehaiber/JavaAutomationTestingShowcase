8337d34c00104b3783ed257d0d1df3393281f464
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-06 12:17:22 -0800

    Created Page Model for the Checkout: Information page Added ability to check if the title of the page is present

---
+++ src/main/java/com/saucedemo/pages/CheckoutInfoPage.java
@@ -0,0 +1,17 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutInfoPage extends BasePage {
+
+    private static final By title = By.cssSelector("[class='title']");
+
+    public CheckoutInfoPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean checkIfTitleIsDisplayed() {
+        return driver.findElement(title).isDisplayed();
+    }
+}
