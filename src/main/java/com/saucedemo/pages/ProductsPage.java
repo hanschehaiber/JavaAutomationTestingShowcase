175c4c6a287271c57504c9013417b2c60b096acb
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-05 22:12:11 -0800

    Updated page object to initialize a list of WebElements representing the entire inventory list upon instantiation of the class.

--- src/main/java/com/saucedemo/pages/ProductsPage.java
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -1,11 +1,21 @@
 package com.saucedemo.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ProductsPage {
     private WebDriver driver;
 
+    By inventoryItems = By.cssSelector("[class='inventory_list'] > div");
+    List<WebElement> inventoryList;
+
     public ProductsPage(WebDriver driver) {
         this.driver = driver;
+        inventoryList = driver.findElements(inventoryItems);
     }
+
+
 }
