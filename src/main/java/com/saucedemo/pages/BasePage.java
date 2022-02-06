857618b3fb7cd922107d6fccf8d4d09cdab942ff
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-06 11:19:45 -0800

    Created abstract BasePage for shared page functionality

---
+++ src/main/java/com/saucedemo/pages/BasePage.java
@@ -0,0 +1,19 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+    WebDriver driver;
+
+    private static final By shoppingCart = By.cssSelector("[class='shopping_cart_link']");
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public ShoppingCartPage clickShoppingCart() {
+        driver.findElement(shoppingCart).click();
+        return new ShoppingCartPage(driver);
+    }
+}
