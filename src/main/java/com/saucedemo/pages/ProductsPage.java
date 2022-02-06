03038b7f0276129dcf951a9231e9a54fdfd5d073
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-06 11:49:18 -0800

    Get price of an item by its index add item to cart based on its index

--- src/main/java/com/saucedemo/pages/ProductsPage.java
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -6,25 +6,28 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-public class ProductsPage {
-    private WebDriver driver;
+public class ProductsPage extends BasePage {
 
     private static final By inventoryItems = By.cssSelector("[class='inventory_list'] > div");
     private static final By itemPrice = By.cssSelector("[class='inventory_item_price']");
+    private static final By addToCart = By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']");
     List<WebElement> inventoryList;
 
     public ProductsPage(WebDriver driver) {
+        super(driver);
         this.driver = driver;
         inventoryList = this.driver.findElements(inventoryItems);
     }
 
-    public String getPriceByIndex(int index) {
-        return inventoryList.get(index).findElement(itemPrice).getText();
+    public double getPriceByIndex(int index) {
+        String priceStr = inventoryList.get(index).findElement(itemPrice).getText().trim();
+        priceStr = priceStr.replace("$", "");
+        return Double.parseDouble(priceStr);
     }
 
-
-
-
+    public void clickAddToCartByIndex(int index) {
+        inventoryList.get(index).findElement(addToCart).click();
+    }
 
 
 }
