cb4ba63654278d10079519d0f0174bca2486344d
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-06 13:38:39 -0800

    Added up to getting to the checkout info page and verifying if that page is displayed via the page's title

--- src/main/java/com/saucedemo/tests/PurchasingItemsTest.java
+++ src/main/java/com/saucedemo/tests/PurchasingItemsTest.java
@@ -1,7 +1,9 @@
 package com.saucedemo.tests;
 
+import com.saucedemo.pages.CheckoutInfoPage;
 import com.saucedemo.pages.LoginPage;
 import com.saucedemo.pages.ProductsPage;
+import com.saucedemo.pages.ShoppingCartPage;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -21,6 +23,14 @@ public class PurchasingItemsTest extends TestBase {
 
 
         ProductsPage productsPage = loginPage.clickLogin(driver);
+        double firstItemPrice = productsPage.getPriceByIndex(0);
+        productsPage.clickAddToCartByIndex(0);
+        ShoppingCartPage shoppingCartPage = productsPage.clickShoppingCart();
+
+        softly.assertEquals(shoppingCartPage.checkNumOfItemsInCart(), 1);
+
+        CheckoutInfoPage checkoutInfoPage = shoppingCartPage.clickCheckout();
+        softly.assertTrue(checkoutInfoPage.checkIfTitleIsDisplayed());
 
 
 
