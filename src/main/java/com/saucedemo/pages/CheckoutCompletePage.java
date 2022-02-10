e1e7dff352d9d84ed341812f4b806674a4df2eb9
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-09 21:25:01 -0800

    Checkout Complete Page Model Updated check if title and get completed message text added

--- src/main/java/com/saucedemo/pages/CheckoutCompletePage.java
+++ src/main/java/com/saucedemo/pages/CheckoutCompletePage.java
@@ -1,4 +1,21 @@
 package com.saucedemo.pages;
 
-public class CheckoutCompletePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutCompletePage extends BasePage {
+    private static final By checkoutCompleteContainer = By.id("checkout_complete_container");
+    private static final By completeText = By.cssSelector("[class='complete-text']");
+
+    public CheckoutCompletePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean checkIfCheckoutCompleteContainerIsDisplayed() {
+        return getDriver().findElement(checkoutCompleteContainer).isDisplayed();
+    }
+
+    public String getCompleteText() {
+        return getDriver().findElement(completeText).getText();
+    }
 }
