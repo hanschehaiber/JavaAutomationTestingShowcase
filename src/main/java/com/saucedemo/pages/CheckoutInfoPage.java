082f7ad2ec925fef3ecbfe72e837e393e75442d8
hanschehaiber <hans.chehaiber@gmail.com>
2022-02-09 20:39:25 -0800

    Updated Checkout Info Page Model The form can be filled out and submitted, with page chaining to next page in the control flow of the application

--- src/main/java/com/saucedemo/pages/CheckoutInfoPage.java
+++ src/main/java/com/saucedemo/pages/CheckoutInfoPage.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.WebDriver;
 public class CheckoutInfoPage extends BasePage {
 
     private static final By title = By.cssSelector("[class='title']");
+    private static final By firstName = By.id("first-name");
+    private static final By lastName = By.id("last-name");
+    private static final By postalCode = By.id("postal-code");
+    private static final By continueButton = By.id("continue");
 
     public CheckoutInfoPage(WebDriver driver) {
         super(driver);
@@ -13,5 +17,22 @@ public class CheckoutInfoPage extends BasePage {
 
     public boolean checkIfTitleIsDisplayed() {
         return getDriver().findElement(title).isDisplayed();
+    }
+
+    public void enterFirstName(String firstNameText) {
+        getDriver().findElement(firstName).sendKeys(firstNameText);
+    }
+
+    public void enterLastName(String lastNameText) {
+        getDriver().findElement(lastName).sendKeys(lastNameText);
+    }
+
+    public void enterPostalCode(String postalCodeText) {
+        getDriver().findElement(postalCode).sendKeys(postalCodeText);
+    }
+
+    public CheckoutOverviewPage clickContinue() {
+        getDriver().findElement(continueButton).click();
+        return new CheckoutOverviewPage(getDriver());
     }
 }
