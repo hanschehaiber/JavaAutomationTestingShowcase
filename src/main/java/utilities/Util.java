e1bb9810472b6d95c3e558a460f17f4dcce27aa2
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2023-07-05 23:04:54 -0700

    added some comments

--- src/main/java/utilities/Util.java
+++ src/main/java/utilities/Util.java
@@ -1,6 +1,10 @@
 package utilities;
 
 public class Util {
+    /**
+     * This is for debugging purposes only. Hard waits should almost never be present in a test.
+     * @param milliSeconds
+     */
     public static void sleep(long milliSeconds) {
         try {
             Thread.sleep(milliSeconds);
