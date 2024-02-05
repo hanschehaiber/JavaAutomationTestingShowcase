7993a3c09cb232053ad30ddb1087f8a536ee70af
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2024-02-05 17:38:20 -0500

    Removed dependency on 3rd part Selenium Driver manager to rely on native SeleniumManager

--- src/main/java/com/saucedemo/tests/TestBase.java
+++ src/main/java/com/saucedemo/tests/TestBase.java
@@ -1,6 +1,5 @@
 package com.saucedemo.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
@@ -27,7 +26,6 @@ public class TestBase {
 
     @BeforeSuite
     public void setup() {
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
