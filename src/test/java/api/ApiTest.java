00ace04e47c90fae6d7da7a252704626e9099212
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2024-02-12 16:29:03 -0500

    Added Soft Assertion for Squat Reps

--- src/test/java/api/ApiTest.java
+++ src/test/java/api/ApiTest.java
@@ -1,12 +1,16 @@
 package api;
 
+import com.fakefitnesswebapp.domain.Exercise;
 import com.fakefitnesswebapp.domain.Workout;
 import io.restassured.response.Response;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.Assert;
+import org.assertj.core.api.SoftAssertions;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
@@ -18,9 +22,12 @@ public class ApiTest {
 
     @Test
     public void testFitnessAppApi() {
+        SoftAssertions softly = new SoftAssertions();
+
         String requestPath = "workout.json";
         String baseUrl = "http://localhost";
         String endpointUrl = "/exercises";
+
         stubFor(get(urlEqualTo(endpointUrl))
                 .willReturn(aResponse()
                         .withStatus(200)
@@ -34,12 +41,16 @@ public class ApiTest {
                 then().extract().response();
 
         Workout workout = response.getBody().as(Workout.class);
+        List<Exercise> exerciseList = workout.getExercises();
+        for(Exercise exercise: exerciseList) {
+            if(exercise.getName().equals("Squats")) {
+                softly.assertThat(exercise.getReps())
+                        .as("Squat Reps")
+                        .isEqualTo(12);
+            }
+        }
 
-
-        System.out.println("WORKOUT = " + workout.toString());
-
-        System.out.println(response.getBody().prettyPrint());
-
+        softly.assertAll();
     }
 
     @Test
