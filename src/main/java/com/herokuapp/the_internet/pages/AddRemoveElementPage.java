e295ac9e1c4d27617a6ae41e59ebfd1fab2c8045
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2024-03-20 15:30:37 -0400

    Finished adding the first testcase for herokuapp

--- src/main/java/com/herokuapp/the_internet/pages/AddRemoveElementPage.java
+++ src/main/java/com/herokuapp/the_internet/pages/AddRemoveElementPage.java
@@ -5,13 +5,18 @@ import org.openqa.selenium.WebDriver;
 
 public class AddRemoveElementPage extends BasePage  {
 
-    private By addElementButton = By.linkText("Add Element");
+    private By addElementButton = By.xpath("//button[contains(text(), 'Add Element')]");
+    private By addedElements = By.id("elements");
     public AddRemoveElementPage(WebDriver driver) {
         super(driver);
     }
 
     public void clickAddElementButton() {
         driver.findElement(addElementButton).click();
+    }
+
+    public int countHowManyAddedElements() {
+        return driver.findElements(addedElements).size();
     }
 
 }
--- src/test/java/com/herokuapp/the_internet/ShowcaseTest.java
+++ src/test/java/com/herokuapp/the_internet/ShowcaseTest.java
@@ -1,8 +1,10 @@
 package com.herokuapp.the_internet;
 
+import com.herokuapp.the_internet.pages.AddRemoveElementPage;
 import com.herokuapp.the_internet.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ShowcaseTest {
@@ -11,8 +13,12 @@ public class ShowcaseTest {
     public void addRemoveElementsTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         HomePage homePage = new HomePage(driver);
+        AddRemoveElementPage addRemoveElementPage = new AddRemoveElementPage(driver);
         driver.get("https://the-internet.herokuapp.com/");
         homePage.clickAddRemoveElements();
+        addRemoveElementPage.clickAddElementButton();
+        Assert.assertEquals(addRemoveElementPage.countHowManyAddedElements(), 1);
+
         Thread.sleep(5000);
 
         driver.quit();
